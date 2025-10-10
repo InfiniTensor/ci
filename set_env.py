375e579d64ef458bd6874204103c711496067f1e
yosure <yangshuo@xcoresigma.com>
2025-10-10 16:11:21 +0800

    set_env脚本添加gpu和cmd参数

--- set_env.py
+++ set_env.py
@@ -12,6 +12,8 @@ parser.add_argument('--email', type=str)
 parser.add_argument('--env', type=str)
 parser.add_argument('--url', type=str)
 parser.add_argument('--model', type=str)
+parser.add_argument('--gpu', type=str)
+parser.add_argument('--cmd', type=str)
 
 args=parser.parse_args()
 file_name = args.file
@@ -19,6 +21,8 @@ emails = args.email
 env = args.env
 url = args.url
 model = args.model
+gpu = args.gpu
+cmd = args.cmd
 
 add_cmd = f"dynaconf write toml -v BASE_URL={url} -v MODEL={model} -v API_KEY=- -p config/env_settings.toml -e {env} -y"
 subprocess.run(add_cmd, shell=True)
