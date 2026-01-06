8076a242406c997bd3a326a0ecc84cb7b36cbb88
limingge <limingge@xcoresigma.com>
2026-01-06 19:21:49 +0800

    ci test

---
+++ ascend_test_suite/deamon.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -m
+
+cleanup() {
+    trap - SIGINT SIGTERM SIGHUP SIGPIPE
+    kill -SIGTERM -$CHILD_PID
+    exit 130
+}
+
+# trap "trap - SIGINT SIGTERM SIGHUP SIGPIPE && kill -- -$$" SIGINT SIGTERM SIGHUP SIGPIPE
+trap cleanup SIGINT SIGTERM SIGHUP SIGPIPE
+
+./ascend_resource_monitor.sh $@ &
+CHILD_PID=$!
+
+while kill -0 $CHILD_PID 2>/dev/null; do
+    sleep 1
+done
