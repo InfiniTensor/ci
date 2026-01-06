23d41472742f6f9cfa82b2fb9eabe2fd379db172
limingge <limingge@xcoresigma.com>
2026-01-06 20:09:28 +0800

    updates

--- ascend_test_suite/deamon.sh
+++ ascend_test_suite/deamon.sh
@@ -2,7 +2,6 @@
 set -m
 
 cleanup() {
-    touch ci_test.txt
     trap - SIGINT SIGTERM SIGHUP SIGPIPE
     kill -SIGTERM -$CHILD_PID
     exit 130
@@ -13,7 +12,9 @@ trap cleanup SIGINT SIGTERM SIGHUP SIGPIPE
 ./ascend_resource_monitor.sh $@ &
 CHILD_PID=$!
 
+echo -n "Running"
 while kill -0 $CHILD_PID 2>/dev/null; do
-    echo "Running..."
+    # echo -ne "\r\033[KRunning..."
+    echo -n "."
     sleep 1
 done
