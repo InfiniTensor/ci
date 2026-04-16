7636a6b0993194382c1d56a49968de9bab387a6c
Vincent Li <limingge@xcoresigma.com>
2026-04-16 11:47:25 +0000

    update

--- ascend_test_suite/daemon.sh
+++ ascend_test_suite/daemon.sh
@@ -5,6 +5,7 @@ cleanup() {
     trap - SIGINT SIGTERM SIGHUP SIGPIPE
     echo "Stopping CI test job..."
     docker stop --timeout 60 CI_test_job_${platform}_${test_type}_${CI_job_id}
+    docker stop --time 60 CI_test_job_${platform}_${test_type}_${CI_job_id}
     # docker kill --signal=SIGTERM CI_test_job_${CI_job_id}
     # docker kill -s TERM CI_test_job_${CI_job_id}
     # rm -rf $curr_dir
@@ -29,7 +30,7 @@ fi
 
 curr_dir=$(pwd)
 
-docker run --rm --name="CI_test_job_${platform}_${test_type}_${CI_job_id}" --ipc=host --net=host --privileged -v /home/zkjh/.npu_locks:/home/zkjh/.npu_locks -v /home/zkjh/CI_Workspace:/CI_Workspace -v /data/shared/limingge/artifacts:/artifacts -v /var/run/docker.sock:/var/run/docker.sock auto-test:latest $platform $test_type $engine $model_list "$docker_args" $CI_job_id $test_param $version &
+docker run --rm --name="CI_test_job_${platform}_${test_type}_${CI_job_id}" --ipc=host --net=host --privileged -v /home/zkjh/.npu_locks:/home/zkjh/.npu_locks -v /home/zkjh/CI_Workspace:/CI_Workspace -v /data/shared/limingge/artifacts:/artifacts -v ~/.ssh:/root/.ssh -v /var/run/docker.sock:/var/run/docker.sock auto-test:latest $platform $test_type $engine $model_list "$docker_args" $CI_job_id $test_param $version &
 CHILD_PID=$!
 
 echo -n "Running"
