c573bfbc405f997f56b386dffeee27d98fbba6ed
Vincent Li <limingge@xcoresigma.com>
2026-04-30 09:17:26 +0000

    updates

--- third-party/scheduler/moore_test_suite/script_generator_for_InfiniTensor.py
+++ third-party/scheduler/moore_test_suite/script_generator_for_InfiniTensor.py
@@ -122,7 +122,7 @@ def main():
                 if test_type == "Unit":
                     lines[line_num] = line.replace("<<<DOCKER_ARGS>>>", docker_args)
                 else:
-                    lines[line_num] = line.replace("<<<DOCKER_ARGS>>>", docker_args + " -e MTHREADS_VISIBLE_DEVICES=$MTHREADS_VISIBLE_DEVICES docker.infinitensor.com/docker/infiniTensor-aarch64-moore:$LATEST_TAG")
+                    lines[line_num] = line.replace("<<<DOCKER_ARGS>>>", docker_args + " -e MTHREADS_VISIBLE_DEVICES=$MTHREADS_VISIBLE_DEVICES -e CUDA_VISIBLE_DEVICES=$MTHREADS_VISIBLE_DEVICES docker.infinitensor.com/docker/infiniTensor-aarch64-moore:$LATEST_TAG")
             line_num += 1
 
         with open(f"{curr_dir}/{target_file}", 'w') as file:
