d812c7d4b47882974b68ee2a6e05d49f7a46845d
Vincent Li <limingge@xcoresigma.com>
2026-05-28 10:19:12 +0000

    updates

--- third-party/scheduler/nvidia_test_suite/nvidia_resource_monitor.sh
+++ third-party/scheduler/nvidia_test_suite/nvidia_resource_monitor.sh
@@ -515,7 +515,7 @@ if [ $TEST_TYPE != "Service" ]; then
             echo "Idle GPU(s) satisfying the conditions have been found, Inference Test will begin..."
             echo
             if [ $TEST_TYPE == "Inference" ]; then
-                inference_log=$curr_dir/logs/inference/$SESSION_ID/cron_job_${GPU_QUANTITY}_${log_name_suffix}_0.log
+                inference_log=$curr_dir/logs/inference/$SESSION_ID/cron_job_${TEST_PARAM// /_}_${log_name_suffix}_0.log
                 $curr_dir/infiniLM_nvidia_test.sh 1 "${servers[*]}" ${model} 0 ${TEST_TYPE} ${ENGINE_TYPE} ${SESSION_ID} "${TEST_PARAM}" ${version} > $inference_log 2>&1 &
             else
                 test_type=$(echo "${TEST_TYPE}" | tr '[:upper:]' '[:lower:]')
