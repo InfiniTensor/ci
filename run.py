bc5f61cd139c31073d6934428a0ac81ac7658f44
Vincent Li <limingge@xcoresigma.com>
2026-06-16 11:50:01 +0000

    updates

--- run.py
+++ run.py
@@ -361,7 +361,7 @@ def main():
 
     config = load_config(args.config)
     repo = config.get("repo", {})
-    repo_url = repo.get("url", "https://github.com/Vincent777/InfiniLM.git")
+    repo_url = repo.get("url", "https://github.com/InfiniTensor/InfiniLM.git")
     branch = args.branch or repo.get("branch", "master")
 
     platform = detect_platform()
